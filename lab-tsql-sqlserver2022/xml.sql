4ace958e552eb2e4791450e7f1fedc0558781d4c
HenriqueCsar <henrique.fibonacci@hotmail.com>
2023-02-27 20:29:37 -0300

    Update LAB-BI V20230227

--- lab-tsql-sqlserver2022/xml.sql
+++ lab-tsql-sqlserver2022/xml.sql
@@ -7,8 +7,6 @@ SELECT distinct top 100 try_cast(
         SUBSTRING(resposta, 1, CHARINDEX('</CodigoOperacao>', resposta) + 16)+
         '</ResultadoXml></SetPolicyEvalValuesObjectXmlResponse></soap:Body></soap:Envelope>'
      as xml).value('(/soap:Envelope/soap:Body/ns:SetPolicyEvalValuesObjectXmlResponse/ns2:ResultadoXml/ns2:CodigoOperacao)[1]', 'VARCHAR(10)') AS CodigoOperacao,
-	Propostaid,
-	b.cpf
 
 FROM tabela a with(nolock)
 where resposta like '%operacao%'
