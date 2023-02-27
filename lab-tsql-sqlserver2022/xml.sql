d9b98aa8ebefae1f4e0eaf929f8534ad654fdfd5
HenriqueCsar <henrique.fibonacci@hotmail.com>
2023-02-27 20:27:44 -0300

    Update LAB-BI V20230227

---
+++ lab-tsql-sqlserver2022/xml.sql
@@ -0,0 +1,15 @@
+# /* Reposta */
+
+WITH XMLNAMESPACES('' AS soap,
+                   '' AS ns,
+                   '' AS ns2)
+SELECT distinct top 100 try_cast(
+        SUBSTRING(resposta, 1, CHARINDEX('</CodigoOperacao>', resposta) + 16)+
+        '</ResultadoXml></SetPolicyEvalValuesObjectXmlResponse></soap:Body></soap:Envelope>'
+     as xml).value('(/soap:Envelope/soap:Body/ns:SetPolicyEvalValuesObjectXmlResponse/ns2:ResultadoXml/ns2:CodigoOperacao)[1]', 'VARCHAR(10)') AS CodigoOperacao,
+	Propostaid,
+	b.cpf
+
+FROM externo.consulta a with(nolock)
+join proposta b with(nolock) on b.id = a.propostaid
+where resposta like '%operacao%'
