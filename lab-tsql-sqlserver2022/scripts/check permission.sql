bf9edd9b1c0994b61da8335ce296b8e9583d21ac
HenriqueCsar <henrique.fibonacci@hotmail.com>
2023-03-15 11:20:22 -0300

    Update LAB-BI V20230315

--- lab-tsql-sqlserver2022/scripts/check permission.sql
+++ lab-tsql-sqlserver2022/scripts/check permission.sql
@@ -1,3 +1,8 @@
 /*Check user permission on database*/
 
 SELECT * FROM fn_my_permissions('MalikAr', 'USER');  
+
+EXECUTE AS USER = 'Wanida';  
+SELECT * FROM fn_my_permissions('HumanResources.Employee', 'OBJECT')   
+    ORDER BY subentity_name, permission_name ;    
+REVERT;  
